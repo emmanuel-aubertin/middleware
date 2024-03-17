8802a15d363a3f0c05ebcd6e5344338b57239772
Emmanuel Aubertin <aubertin.emmanuel@gmail.com>
2024-03-17 15:43:55 +0100

    👍 install dependices for ubuntu 22.04 👍

---
+++ install.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt-get install libssl-dev build-essential libbz2-dev
+
+sudo apt install vlc libvlc-dev
+
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 5E6DA83306132997
+sudo apt-add-repository "deb http://zeroc.com/download/Ice/3.7/ubuntu20.04 stable main"
+
+
+sudo apt update
+sudo apt install zeroc-ice-all-runtime zeroc-ice-all-dev
+
+
+sudo apt install python3-pip
+pip3 install zeroc-ice
